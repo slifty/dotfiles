cde1167cea8e7192ff0c0db6bbf5b1b7f9db40d0
Dan Schultz <slifty@gmail.com>
2018-03-13 16:16:57 -0400

    Set up Package Control automatically
    It needs to be manually added since it is the thing that knows how
    to add packages automatically.

--- sublime.symlink/install.sh
+++ sublime.symlink/install.sh
@@ -1,5 +1,7 @@
 echo "SeTtInG Up SuBlImE."
-ln -s ".sublime/Packages" "~/Library/Application Support/Sublime Text 3/Packages"
+rm -fr ~/Library/Application\ Support/Sublime\ Text 3/Packages
+mv Package\ Control.sublime-package ~/Library/Application\ Support/Sublime\ Text 3/Installed\ Packages
+ln -s .sublime/Packages ~/Library/Application\ Support/Sublime\ Text 3
 
 echo "SeTtInG Up CoDeInTeL."
 sudo pip install --upgrade --pre --ignore-installed CodeIntel
