1f4bc2aa7c28665e36132289ebc706dae47dea26
Dan Schultz <slifty@gmail.com>
2018-03-13 16:07:15 -0400

    CodeIntel wouldn't install twice
    This generally doesn't matter but ideally things will just work
    regardless of existing installed state.

--- sublime.symlink/install.sh
+++ sublime.symlink/install.sh
@@ -2,4 +2,4 @@ echo "SeTtInG Up SuBlImE."
 ln -s ".sublime/Packages" "~/Library/Application Support/Sublime Text 3/Packages"
 
 echo "SeTtInG Up CoDeInTeL."
-sudo pip install --upgrade --pre CodeIntel
+sudo pip install --upgrade --pre --ignore-installed CodeIntel
