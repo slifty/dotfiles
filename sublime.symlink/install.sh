f7592764619c4b683c8ecd24b0a7047a54f6d88b
Dan Schultz <slifty@gmail.com>
2018-03-13 15:49:15 -0400

    actually prevent infinite recursion
    idk what I did in the last commit tbh

--- sublime.symlink/install.sh
+++ sublime.symlink/install.sh
@@ -1,9 +1,5 @@
-if test ! $(which spoof)
-then
-  rm ".sublime/install.sh"
-  echo "SeTtInG Up SuBlImE."
-  link_file ".sublime/Packages" "~/Library/Application Support/Sublime Text 3/Packages"
+echo "SeTtInG Up SuBlImE."
+link_file ".sublime/Packages" "~/Library/Application Support/Sublime Text 3/Packages"
 
-  echo "SeTtInG Up CoDeInTeL."
-  pip install --upgrade --pre CodeIntel
-fi
+echo "SeTtInG Up CoDeInTeL."
+pip install --upgrade --pre CodeIntel
