b66d59411ad21374e34a50366ce5d74002bd6f21
Dan Schultz <slifty@gmail.com>
2018-03-13 15:58:24 -0400

    CodeIntel needs sudo I guess
    Feels like I shouldn't use sudo but I mean, russia has infiltrated the US government
    and we're all going to die anyway so does it really matter.

--- sublime.symlink/install.sh
+++ sublime.symlink/install.sh
@@ -1,5 +1,5 @@
 echo "SeTtInG Up SuBlImE."
-link_file ".sublime/Packages" "~/Library/Application Support/Sublime Text 3/Packages"
+ln -s ".sublime/Packages" "~/Library/Application Support/Sublime Text 3/Packages"
 
 echo "SeTtInG Up CoDeInTeL."
-pip install --upgrade --pre CodeIntel
+sudo pip install --upgrade --pre CodeIntel
