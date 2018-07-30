a6bb13297e6dcedf51a64bffb1d0f0969abf13f5
Dan Schultz <slifty@gmail.com>
2018-07-30 12:21:27 -0400

    Kill CodeIntel
    It was a nice idea, but it doesn't really install.

--- sublime.symlink/install.sh
+++ sublime.symlink/install.sh
@@ -3,5 +3,5 @@ rm -fr ~/Library/Application\ Support/Sublime\ Text 3/Packages
 mv ~/.sublime/Package\ Control.sublime-package ~/Library/Application\ Support/Sublime\ Text 3/Installed\ Packages
 ln -s ~/.sublime/Packages ~/Library/Application\ Support/Sublime\ Text 3/Packages
 
-echo "SeTtInG Up CoDeInTeL."
-sudo pip install --upgrade --pre --ignore-installed CodeIntel
+# echo "SeTtInG Up CoDeInTeL."
+# sudo pip install --upgrade --pre --ignore-installed CodeIntel
