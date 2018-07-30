e3bf873706c8655d3e5e7e92d66bf1213ea71ec4
Dan Schultz <slifty@gmail.com>
2018-07-30 12:31:18 -0400

    Escape space in sublime path
    HOW DID THIS EVER WORK. WHAT IS GOING ON.

--- sublime.symlink/install.sh
+++ sublime.symlink/install.sh
@@ -1,7 +1,7 @@
 echo "SeTtInG Up SuBlImE."
-rm -fr ~/Library/Application\ Support/Sublime\ Text 3/Packages
-mv ~/.sublime/Package\ Control.sublime-package ~/Library/Application\ Support/Sublime\ Text 3/Installed\ Packages
-ln -s ~/.sublime/Packages ~/Library/Application\ Support/Sublime\ Text 3/Packages
+rm -fr ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+mv ~/.sublime/Package\ Control.sublime-package ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
+ln -s ~/.sublime/Packages ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
 
 # echo "SeTtInG Up CoDeInTeL."
 # sudo pip install --upgrade --pre --ignore-installed CodeIntel
