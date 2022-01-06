52d9f1063947cef600d0ddbcd27ec25e7d786a62
Dan Schultz <slifty@gmail.com>
2022-01-05 22:35:58 -0500

    Goodbye Sublime
    I loved sublime, it was a wonderful editor and I used it for most of my
    development career.  I've decided to move to VSCode for a few reasons
    
    1. It's FOSS
    2. It has better native support for things like TypeScript
    3. It has a very robust plugin ecosystem
    4. It does settings sync via cloud (My dotfiles attempt at setting sync
       never *really* worked).
    
    I wish great things for the future of Sublime even if I'm not using it
    any longer.

--- sublime.symlink/install.sh
+++
@@ -1,4 +0,0 @@
-echo "Setting up Sublime."
-rm -fr ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
-mv ~/.sublime/Package\ Control.sublime-package ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
-ln -s ~/.sublime/Packages ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
