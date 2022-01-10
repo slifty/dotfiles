d22c32aac5e727839b39a0d1c62e7e49f712ded0
Dan Schultz <slifty@gmail.com>
2022-01-09 21:35:20 -0500

    Configure desktop background
    I can't believe how hard this was!
    
    This should do two things:
    1. Add a link to the dotfiles list of desktop backgrounds to the
       preferences pane.
    2. Load the one I like these days as the default

--- macos/install.sh
+++ macos/install.sh
@@ -2,13 +2,12 @@
 # still be updates and installables in the Mac App Store. There's a nifty
 # command line interface to it that we can use to just install everything, so
 # yeah, let's do that.
-
-echo "› sudo softwareupdate -i -a"
+printf "\e[32mRunning MacOS setup\e[0m\n"
+set -x
 sudo softwareupdate -i -a
 
 # To look up the install ID go to the store and "copy link"
 # and pull it from the URL
-echo "Installing App Store apps"
 mas install 1303222628 # Paprika Recipe Manager 3 (3.2.3)
 mas install 904280696 # Things3 (3.4.1)
 mas install 405399194 # Kindle (1.21.1)
@@ -19,3 +18,15 @@ mas install 506189836 # Harvest (2.1.5)
 mas install 1091189122 # Bear (1.4.3)
 mas install 409183694 # Keynote (8.1)
 mas install 497799835 # XCode (9.4.1)
+
+#######################
+## Desktop Background
+#######################
+# Add the DotfileBackgrounds directory to the preference pane because we're just that detail oriented
+defaults write com.apple.systempreferences DSKDesktopPrefPane "<dict><key>UserFolderPaths</key><array><string>$HOME/.dotfiles/macos/DotfileBackgrounds</string></array></dict>"
+# Set Desktop Background
+osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"$HOME/.dotfiles/macos/DotfileBackgrounds/ZeldaGhib.heic\" as POSIX file"
+
+{ set +x; } 2>/dev/null
+printf "\e[32mMacOS settings updated.\e[0m\n"
+printf "\e[33m(You may need to restart for all settings to take effect)\e[0m\n"
