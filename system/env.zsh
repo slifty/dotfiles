6e35e6c16f8b734fce2fd8748a02e2ede67ddc69
Dan Schultz <slifty@gmail.com>
2022-02-16 14:39:32 -0500

    Add OpenSSH headers
    I'm not sure if this ONLY applies to the python library, but I suspect
    it's useful to have these headers added generally

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,7 @@
 export EDITOR='code'
+
+# set LIBRARY_PATH so it includes openssl if it exists
+# (this is required by certain builds, e.g. python's psycopg2)
+if [ -d /usr/local/opt/openssl/lib ] ; then
+  export LIBRARY_PATH="${LIBRARY_PATH}:/usr/local/opt/openssl/lib"
+fi
