6c5d494f46cbdecb913263ac2ade13c34c3d4c8b
Dan Schultz <slifty@gmail.com>
2022-04-18 18:24:55 -0400

    Auto-load some software
    There is some software I want to run on load; by opening them once it
    should fix that.

--- system/install.sh
+++ system/install.sh
@@ -1,2 +1,6 @@
 # Make sure our bin files are placed where we expect (this is added to path in ./path.zsh)
 ln -s ~/.dotfiles/bin $HOME/bin
+
+# Open things that need to be opened once in order to boot on load
+open /Applications/Rectangle.app
+open /Applications/SoundSource.app
