96d664abbbe4630f0dcb1d99f27428ea94fd2f72
Dan Schultz <slifty@gmail.com>
2022-04-18 18:23:28 -0400

    Update homebrew install
    The ruby script is obsolete; this uses the new shiny bash script.

--- homebrew/preinstall.sh
+++ homebrew/preinstall.sh
@@ -11,7 +11,7 @@ then
   echo "Installing Homebrew"
 
   # Install the correct homebrew for each OS type
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"  
   git -C "/usr/local/Homebrew/Library/Taps/homebrew/homebrew-core" fetch --unshallow
   git -C "/usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask" fetch --unshallow
 fi
