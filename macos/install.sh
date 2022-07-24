05c758509465d740a816c76f3240091b92e03c3d
Dan Schultz <slifty@gmail.com>
2022-07-24 12:29:53 -0400

    Upgrade to Pixelmator Pro
    Pixelmator apparently isn't supported any more (also pro is pretty great
    turns out)

--- macos/install.sh
+++ macos/install.sh
@@ -12,7 +12,7 @@ mas install 411643860 # DaisyDisk (4.4)
 mas install 1303222628 # Paprika Recipe Manager 3 (3.2.3)
 mas install 904280696 # Things3 (3.4.1)
 mas install 405399194 # Kindle (1.21.1)
-mas install 407963104 # Pixelmator (3.7)
+mas install 1289583905 # Pixelmator Pro
 mas install 992076693 # MindNode (2.5.7)
 mas install 880001334 # Reeder (3.1.2)
 mas install 506189836 # Harvest (2.1.5)
@@ -20,7 +20,6 @@ mas install 1091189122 # Bear (1.4.3)
 mas install 409183694 # Keynote (8.1)
 mas install 497799835 # XCode (9.4.1)
 mas install 1236045954 # Canary Mail ()
-
 
 #############
 ## Set some defaults
