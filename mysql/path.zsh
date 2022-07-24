495a9dc5a2812a68c8b2b152640d7898d8599c0f
Dan Schultz <slifty@gmail.com>
2022-07-24 12:36:28 -0400

    Support ARM Macs
    ARM based macs have different file locations for whatever reason; this
    tries to pull from the right places when on an ARM mac

--- mysql/path.zsh
+++ mysql/path.zsh
@@ -1,1 +1,8 @@
-export PATH="/usr/local/opt/mysql-client/bin:$PATH"
+if [ -d /usr/local/opt/mysql-client/bin ] ; then
+	export PATH="/usr/local/opt/mysql-client/bin:$PATH"
+fi
+
+# M1 macs use a different path
+if [ -d /opt/homebrew/opt/mysql-client/bin ] ; then
+	export PATH="/opt/homebrew/opt/mysql-client/bin:$PATH"
+fi
--- system/env.zsh
+++ system/env.zsh
@@ -5,3 +5,8 @@ export EDITOR='code'
 if [ -d /usr/local/opt/openssl/lib ] ; then
   export LIBRARY_PATH="${LIBRARY_PATH}:/usr/local/opt/openssl/lib"
 fi
+
+# M1 macs put this stuff in a different place; support that if it exists
+if [ -d /opt/homebrew/opt/openssl/lib ] ; then
+  export LIBRARY_PATH="${LIBRARY_PATH}:/opt/homebrew/opt/openssl/lib"
+fi
