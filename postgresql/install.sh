5cf7e108d4560e68338f0c8f6009d5495dcb6023
Dan Schultz <slifty@gmail.com>
2023-06-19 22:48:28 -0400

    Add debug notes to psql

--- postgresql/install.sh
+++ postgresql/install.sh
@@ -1,2 +1,10 @@
 echo "Starting postgresql service"
 brew services start postgresql
+
+# Since I always come here when psql is not working as expected
+# here are some useful extra commands:
+#
+# brew services restart -v postgresql
+# brew services info -a
+# brew postgresql-upgrade-database
+# tail /opt/homebrew/var/log/postgres.log
