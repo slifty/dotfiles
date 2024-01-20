e2dab8f64ba1c698536521c60349f023fb98e48f
Dan Schultz <slifty@gmail.com>
2024-01-19 21:30:05 -0500

    Add more psql debugging info

--- postgresql/install.sh
+++ postgresql/install.sh
@@ -8,3 +8,6 @@ brew services start postgresql
 # brew services info -a
 # brew postgresql-upgrade-database
 # tail /opt/homebrew/var/log/postgres.log
+#
+# If the postmaster.pid exists when it shouldn't (e.g. forced reboot)
+# its location will be /opt/homebrew/var/postgres/postmaster.pid
